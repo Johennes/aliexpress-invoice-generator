4b45ee501308728342772db14d3e374816283a49
Johannes Marbach <n0-0ne+gitlab@mailbox.org>
2019-12-21 21:07:46 +0100

    Replace run and package scripts with web-ext

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ ! -f pdfkit.standalone.js ]]; then
-    wget https://github.com/foliojs/pdfkit/releases/download/v0.10.0/pdfkit.standalone.js
-fi
-
-npm i
-node_modules/web-ext/bin/web-ext run --verbose
